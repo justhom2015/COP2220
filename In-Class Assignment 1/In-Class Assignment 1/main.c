14d56cc34406a34debe9ddb2bbf1d61599ee54d3
AbirFaisal <abirfaisal@Abirs-MBP.lan>
2015-01-16 02:48:04 -0500

    5

---
+++ In-Class Assignment 1/In-Class Assignment 1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  In-Class Assignment 1
+//
+//  Created by Abir Faisal on 1/15/15.
+//  Copyright (c) 2015 Abir Faisal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
