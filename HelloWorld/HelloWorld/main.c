8e1e57c740a65e54419aa2824932251e76fd2453
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-01-15 19:03:18 -0500

    Hello World

---
+++ HelloWorld/HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Abir Faisal on 1/15/15.
+//  Copyright (c) 2015 Abir Faisal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
