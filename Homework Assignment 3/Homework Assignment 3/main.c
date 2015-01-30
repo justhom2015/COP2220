476f19f29b394ce801927d2b33e1087e37635565
AbirFaisal <abirfaisal@Abirs-MBP.lan>
2015-01-30 03:16:31 -0500

    somewhat works

--- Homework Assignment 3/Homework Assignment 3/main.c
+++ Homework Assignment 3/Homework Assignment 3/main.c
@@ -28,51 +28,81 @@
 
 int main() {
 	
-	const int INPUT_SIZE = 2048;
-	char input[INPUT_SIZE];
+	int INPUT_SIZE = 1024;	//input array size
+	char input[INPUT_SIZE];			//input array
 	
-		//Prompt User
+		// initalize input[] elements to 128
+		// there are 127 characters in ascii table so the value 128 can be used to check for input in the element.
+	for (int i = 0; i < INPUT_SIZE; i++) {
+		input[i]=128;
+	}
+	
+		//Prompt User for input
 	printf("Enter a string less than 1000 characters in length: \n");
 	scanf("%s", input);
 	
-	
 		//Make everything uppercase because in the ASCII Table 'a' and 'A' are different values
 	for (int i = 0; input[i]; i++) {
-		input[i] = toupper(input[i]);
+		input[i] = (int)toupper(input[i]);
 	}
 	
 	
-	const int FREQUENCY_SIZE = 127;
-	int frequency[FREQUENCY_SIZE]; //Compiler will automatically initalize all elements to 0
+	int match[7]; // array to count and store matches
 
-	int currentInputElement = 0;
-	int currentFrequencyElement = 0;
 	
+		//go through array and check if element matches E, T, A, O, I, N, or S
 	for (int i = 0; i < INPUT_SIZE; i++) {
-		
-			// go through all frequency elements and test
-		
-		for (int i = 0; i < FREQUENCY_SIZE; i++) {
-			if (input[currentInputElement] == currentFrequencyElement) {
-				
-				frequency[currentFrequencyElement] = frequency[currentFrequencyElement] + 1;
-			
-				currentFrequencyElement = currentFrequencyElement + 1;
+				//If element matches then add 1 to the value of the element in
+			switch ((int)input[i]) {
+				case (int)'E':
+					match[0] = match[0] + 1;
+					break;
+					
+				case (int)'T':
+					match[1] = match[1] + 1;
+					break;
+					
+				case (int)'A':
+					match[2] = match[2] + 1;
+					break;
+					
+				case (int)'O':
+					match[3] = match[3] + 1;
+					break;
+					
+				case (int)'I':
+					match[4] = match[4] + 1;
+					break;
+					
+				case (int)'N':
+					match[5] = match[5] + 1;
+					break;
+					
+				case (int)'S':
+					match[6] = match[6] + 1;
+					break;
+					
+				default:
+					break;
 			}
-		}
-		
-			//test next element
-			currentInputElement = currentInputElement + 1;
 	}
 	
 	
 	
-	for (int i = 0; i < FREQUENCY_SIZE; i++) {
-		if (frequency[i] ) {
-		printf("%i", (int)frequency[i]);
-		}
+	
+		//Calculate string length by subtracting the number of
+		//elements above 127 from the INPUT_SIZE
+	
+	for (int i=0; i < 7;i++) {
+		printf("%i \n", match[i]);
 	}
 	
 	
+	
+	
+	
+	
+	
+
 	return 0;
 }
