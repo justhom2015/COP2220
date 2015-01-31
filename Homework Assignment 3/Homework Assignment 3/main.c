1832342d0694a0434d079105788cb7865fd17b50
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-01-31 05:06:31 -0500

    test

--- Homework Assignment 3/Homework Assignment 3/main.c
+++ Homework Assignment 3/Homework Assignment 3/main.c
@@ -34,7 +34,7 @@ int main() {
 		// initalize input[] elements to 128
 		// there are 127 characters in ascii table so the value 128 can be used to check for input in the element.
 	for (int i = 0; i < INPUT_SIZE; i++) {
-		input[i]=128;
+		input[i]=127;
 	}
 	
 		//Prompt User for input
@@ -51,6 +51,7 @@ int main() {
 	
 		//go through array and check if element matches E, T, A, O, I, N, or S
 	int match[7]; // array to count and store matches
+	int lettersAsIntegers[6] = { (int)'E' , (int)'T' , (int)'A' , (int)'O' , (int)'I' , (int)'N' , (int)'S'};
 	for (int i = 0; i < INPUT_SIZE; i++) {
 				//If element matches then add 1 to the value of the element in
 			switch ((int)input[i]) {
@@ -87,16 +88,26 @@ int main() {
 			}
 	}
 	
-	
 		//Calculate string length by subtracting the number of
 		//elements above 127 from the INPUT_SIZE
 	
 		//Before we initalized all elements of input[] to 128 since there are 127 characters in the ascii table.
 		//This while loop will end when the element of input[] equals 128.
+		/*
+		 input[]
+		   0   1   2   3   4   5
+		 -------------------------
+		 | A | B | C |127|127|127|
+		 -------------------------
+		 When we detect the number 127 we know we have reached the end of the string.
+		 */
 	int stringLength = 0;
-	while ((int)input[stringLength] != 128) {
+	printf("%i \n", (int)input[0]);
+	while (input[stringLength] != 127) {
 		stringLength++;
 	}
+		//Arrays start with 0 so subtract 1 from stringLength
+	stringLength = stringLength - 1;
 
 		//If user enters more than 1000 characters or didnt enter anything then call main.
 	if (stringLength > 1000) {
@@ -104,13 +115,11 @@ int main() {
 	}else if (stringLength == 0){
 		main();
 	}
-	
-	
-	
+
+		//Go through array that has occurences stores
 	for (int i=0; i < 7;i++) {
-		double frequency = match[i]/stringLength;
-		printf("%f", frequency);
-		printf("%i \n", match[i]);
+		double frequency = (double)match[i]/(double)stringLength;
+		printf("The letter occured %f \n", frequency);
 	}
 	
 
