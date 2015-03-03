0482b1ba8e7c4d3f21f34eaeba434ce44445349b
AbirFaisal <abirfaisal@Abirs-MBP.lan>
2015-03-03 01:27:59 -0500

    Initial Commit

---
+++ Homework Assignment 4/Homework Assignment 4/main.c
@@ -0,0 +1,46 @@
+//
+//  main.c
+//  Homework Assignment 4
+//
+//  Created by Abir Faisal on 3/3/15.
+//  Copyright (c) 2015 Abir Faisal. All rights reserved.
+//
+
+	/////////////////
+	// Assignment: //
+	/////////////////
+/*
+ Review using random functions (srand and rand) in C.
+ 
+ Create a function that takes in the following:
+	- Health – Double
+ 	- Armor – Double from 0 to 1.
+	- Minimum Damage – Unsigned integer.
+	- Maximum Damage – Unsigned integer.
+
+ The function calculates how much damage is dealt to the player.
+	- Use the random functions to pick a random number between the minimum and maximum damage.
+	- Multiply the damage by (1-Armor) and subtract the new value from health.
+	- Output the new health value, how much damage was dealt (double), and a 1 if the player is still alive (health > 0) or 0 if not.
+ 
+ Allow the user to enter the health value, armor value, min damage, and max damage to be used in your function.
+ 	- Run a simulation that calls the function until the player’s health drops below 0.
+	- Every time the function is called, output the results it produces.
+ 
+ HINT: You want to call srand in your main function ONCE.
+ Then call rand() in your new function.
+ 
+ HINT: Your new function will pass some of the variables by reference so that it can also output them.
+ 
+ HINT: In Unit 1->Week 3->Section 2 there was a problem to solve using random numbers.  Go back and review how this works.
+ 
+ You will need to use modulus operators and addition to constrain the number returned by rand() to [min damage, max damage].
+ */
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
