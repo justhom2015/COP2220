78741d46ed56720ba1bf217a1d0f9c7deda3afca
AbirFaisal <abirfaisal@Abirs-MacBook-Pro.local>
2015-03-04 20:08:54 -0500

    Initial Commit

---
+++ Program B: Twin Prime Tester (twinprime.c)/Program B: Twin Prime Tester (twinprime.c)/twinprime.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Program B: Twin Prime Tester (twinprime.c)
+//
+//  Created by Abir Faisal on 3/4/15.
+//  Copyright (c) 2015 Shekh Arefen. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
